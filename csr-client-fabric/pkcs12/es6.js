e510c69becd5a9ee44bc0cffdf863571c053cf19
Christian Felsing <c@felsing.net>
2017-05-17 12:50:35 +0200

    PKCS12 class updated, still not working because of browser issues

--- csr-client-fabric/pkcs12/es6.js
+++ csr-client-fabric/pkcs12/es6.js
@@ -22,16 +22,16 @@ const getBase64FromPEM = function (pem) {
     const endChars = /-----END CERTIFICATE-----|-----END PRIVATE KEY-----/;
     const endLineChars = /(?:\r\n|\r|\n)/g;
 
-    let b64 = pem.replace (startChars, "");
-    b64 = b64.replace (endChars, "");
-    b64 = b64.replace (endLineChars, "");
-    console.log ("pkcs12 pem: %o", b64);
+    let b64 = pem.replace(startChars, "");
+    b64 = b64.replace(endChars, "");
+    b64 = b64.replace(endLineChars, "");
+    console.log("pkcs12 pem: %o", b64);
 
     return b64;
 };
 //*********************************************************************************
-const generateSafeContents = function (keyLocalIDBuffer, certLocalIDBuffer) {
-    const asn1 = asn1js.fromBER(stringToArrayBuffer(fromBase64(getBase64FromPEM(window.csr.privateKey))));
+const generateSafeBags = function () {
+    const asn1 = asn1js.fromBER(stringToArrayBuffer(fromBase64(window.csr.privateKey)));
     const pkcs8Simpl = new PrivateKeyInfo({schema: asn1.result});
 
     const keyLocalIDBuffer = new ArrayBuffer(4);
@@ -57,86 +57,75 @@ const generateSafeContents = function (keyLocalIDBuffer, certLocalIDBuffer) {
         })
     ];
 
-    const safeContents = [
-        {
-            privacyMode: 0, // "No-privacy" Protection Mode
-            value: new SafeContents({
-                safeBags: [
-                    new SafeBag({
-                        bagId: "1.2.840.113549.1.12.10.1.2",
-                        bagValue: new PKCS8ShroudedKeyBag({
-                            parsedValue: pkcs8Simpl
-                        }),
-                        bagAttributes: [
-                            new Attribute({
-                                type: "1.2.840.113549.1.9.20", // friendlyName
-                                values: [
-                                    new asn1js.BmpString({value: window.csr.friendlyName})
-                                ]
-                            }),
-                            new Attribute({
-                                type: "1.2.840.113549.1.9.21", // localKeyID
-                                values: [
-                                    new asn1js.OctetString({valueHex: keyLocalIDBuffer})
-                                ]
-                            }),
-                            new Attribute({
-                                type: "1.3.6.1.4.1.311.17.1", // pkcs12KeyProviderNameAttr
-                                values: [
-                                    new asn1js.BmpString({value: "http://www.pkijs.org"})
-                                ]
-                            })
+    const safeBags = [
+        new SafeBag({
+            bagId: "1.2.840.113549.1.12.10.1.2",
+            bagValue: new PKCS8ShroudedKeyBag({
+                parsedValue: pkcs8Simpl
+            }),
+            bagAttributes: [
+                new Attribute({
+                    type: "1.2.840.113549.1.9.20", // friendlyName
+                    values: [
+                        new asn1js.BmpString({value: window.csr.friendlyName})
+                    ]
+                }),
+                new Attribute({
+                    type: "1.2.840.113549.1.9.21", // localKeyID
+                    values: [
+                        new asn1js.OctetString({valueHex: keyLocalIDBuffer})
+                    ]
+                }),
+                new Attribute({
+                    type: "1.3.6.1.4.1.311.17.1", // pkcs12KeyProviderNameAttr
+                    values: [
+                        new asn1js.BmpString({value: "http://www.pkijs.org"})
+                    ]
+                })
+            ]
+        })
+    ];
+
+    const numCerts = window.csr.certificateChain.length;
+    for (let i = 0; i < numCerts; i++) {
+        const asn1 = asn1js.fromBER(stringToArrayBuffer(fromBase64(window.csr.certificateChain[i])));
+        const certSimpl = new Certificate({schema: asn1.result});
+
+        const certLocalIDBuffer = new ArrayBuffer(4);
+        const certLocalIDView = new Uint8Array(certLocalIDBuffer);
+        getRandomValues(certLocalIDView);
+
+        safeBags.push(
+            new SafeBag({
+                bagId: "1.2.840.113549.1.12.10.1.3",
+                bagValue: new CertBag({
+                    parsedValue: certSimpl
+                }),
+                bagAttributes: [
+                    new Attribute({
+                        type: "1.2.840.113549.1.9.20", // friendlyName
+                        values: [
+                            new asn1js.BmpString({value: window.csr.friendlyName})
+                        ]
+                    }),
+                    new Attribute({
+                        type: "1.2.840.113549.1.9.21", // localKeyID
+                        values: [
+                            new asn1js.OctetString({valueHex: certLocalIDBuffer})
+                        ]
+                    }),
+                    new Attribute({
+                        type: "1.3.6.1.4.1.311.17.1", // pkcs12KeyProviderNameAttr
+                        values: [
+                            new asn1js.BmpString({value: "http://www.pkijs.org"})
                         ]
                     })
                 ]
             })
-        }
-    ];
-
-    //const numCerts = window.csr.certificateChain.length;
-    const numCerts = 1;
-    for (let i=0;i<numCerts;i++) {
-        const asn1 = asn1js.fromBER(stringToArrayBuffer(fromBase64(getBase64FromPEM(window.csr.certificateChain[i]))));
-        const certSimpl = new Certificate({schema: asn1.result});
-
-        safeContents.push(
-            {
-                privacyMode: 1, // Password-Based Privacy Protection Mode
-                value: new SafeContents({
-                    safeBags: [
-                        new SafeBag({
-                            bagId: "1.2.840.113549.1.12.10.1.3",
-                            bagValue: new CertBag({
-                                parsedValue: certSimpl
-                            }),
-                            bagAttributes: [
-                                new Attribute({
-                                    type: "1.2.840.113549.1.9.20", // friendlyName
-                                    values: [
-                                        new asn1js.BmpString({value: window.csr.friendlyName})
-                                    ]
-                                }),
-                                new Attribute({
-                                    type: "1.2.840.113549.1.9.21", // localKeyID
-                                    values: [
-                                        new asn1js.OctetString({valueHex: certLocalIDBuffer})
-                                    ]
-                                }),
-                                new Attribute({
-                                    type: "1.3.6.1.4.1.311.17.1", // pkcs12KeyProviderNameAttr
-                                    values: [
-                                        new asn1js.BmpString({value: "http://www.pkijs.org"})
-                                    ]
-                                })
-                            ]
-                        })
-                    ]
-                })
-            }
         );
     }
 
-    return safeContents;
+    return safeBags;
 };
 //*********************************************************************************
 //endregion 
@@ -154,7 +143,14 @@ function openSSLLikeInternal(password) {
             integrityMode: 0, // Password-Based Integrity Mode
             authenticatedSafe: new AuthenticatedSafe({
                 parsedValue: {
-                    safeContents: generateSafeContents()
+                    safeContents: [
+                        {
+                            privacyMode: 1, // Password-Based Privacy Protection Mode
+                            value: new SafeContents({
+                                safeBags: generateSafeBags()
+                            })
+                        }
+                    ]
                 }
             })
         }
@@ -216,7 +212,7 @@ function openSSLLikeInternal(password) {
 //*********************************************************************************
 function openSSLLike() {
     return Promise.resolve().then(() => openSSLLikeInternal(window.csr.password)).then(result => {
-        console.log ("pkcs#12 password: %o", window.csr.password);
+        console.log("pkcs#12 password: %o", window.csr.password);
         const pkcs12AsBlob = new Blob([result], {type: "application/x-pkcs12"});
         const downloadLink = document.createElement("a");
         downloadLink.download = "pkijs_pkcs12.p12";
